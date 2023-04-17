77bda7401e388cb72ac9c71329e53b307f0c42e5
Safeer Ahmad <72452953+safeerahmad809@users.noreply.github.com>
2023-04-17 20:57:09 +0530

    Update forest.py

--- models/forest.py
+++ models/forest.py
@@ -1,27 +1,28 @@
+# Import necessary libraries
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error, r2_score
 
 # Load the dataset
-data = pd.read_csv('crime_dataset.csv')
+df = pd.read_csv('crime_data.csv')
 
-# Define the features and target variable
-features = ['latitude', 'longitude', 'street_name', 'crime_type', 'month', 'year', 'day', 'hour', 'minute']
-target = 'safest_route'
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df.drop('danger_index', axis=1), df['danger_index'], test_size=0.2, random_state=42)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.2, random_state=42)
+# Initialize the model
+rf = RandomForestRegressor(n_estimators=100, random_state=42)
 
-# Create the RandomForestClassifier model
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+# Train the model on the training set
+rf.fit(X_train, y_train)
 
-# Fit the model to the training data
-model.fit(X_train, y_train)
+# Make predictions on the testing set
+y_pred = rf.predict(X_test)
 
-# Make predictions on the test data
-y_pred = model.predict(X_test)
+# Calculate model performance metrics
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
-# Calculate the accuracy of the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")
+# Print model performance metrics
+print('Mean Squared Error:', mse)
+print('R^2 Score:', r2)
